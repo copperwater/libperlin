6710c1aa64e2bf4cbd77c154440d5806aa0071da
copperwater <aosdict@gmail.com>
2016-09-26 16:15:05 -0400

    Changed stub to behave properly

--- stub.c
+++ stub.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <limits.h>
+#include <time.h>
 #include "perlin.h"
 
 typedef unsigned char uchar;
@@ -9,7 +10,7 @@ int main(int argc, char* argv[]) {
   uchar oend = 7;
   float persistence = 0.5;
   float x,y;
-  mytime = time(NULL);
+  set_perlin_seed(time(0));
 
   //perlin_2d(0.4,0.3,persistence,obegin,oend);
   for(x=-2;x<=2;x+=0.05) {
@@ -24,4 +25,4 @@ int main(int argc, char* argv[]) {
   /**/
   return 0;
 }
-  
+
